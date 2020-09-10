fc7fe910747f9dad815c19d7fcc5eea28228fc52
SummerTold <fabien.petit@univ-amu.fr>
2020-09-10 17:17:58 +0200

    packages in _script folder add loc_base script

---
+++ _script/loc_base.R
@@ -0,0 +1,6 @@
+# Standard file location
+loc_data = file.path(getwd(), "_data")
+loc_function = file.path(getwd(), "_function")
+loc_graphic = file.path(getwd(), "_graphic")
+loc_script = file.path(getwd(), "_script")
+loc_tabular = file.path(getwd(), "_tabular")
