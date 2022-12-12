acdac89bbb97fa9348133ccef641fc0ca025b71f
FabienPetitEconomics <fabien.petit@univ-amu.fr>
2022-12-12 18:21:12 +0000

    clean local url for bibliography, dir parent +2

--- _script/_bib/clean-local-url.R
+++ _script/_bib/clean-local-url.R
@@ -1,4 +1,4 @@
-path <- file.path(dirname(dirname(getwd())), "Downloads")
+path <- file.path(dirname(dirname(dirname(dirname(getwd())))), "Downloads")
 file <- file.path(path, "references.bib")
 readFile <- readLines(file)
 cleanFile <- readFile[-grep("local-url", readFile)]
